d97c85eebbc1c14adeaade6f27cdb3d66b813096
Andrei Santos <andrei.santos@locaweb.com.br>
2022-08-10 08:55:57 -0300

    Solve the chalange

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,15 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list) do
+    lengthCount(list, 0)
+  end
+
+  defp lengthCount([], acc) do
+    acc
+  end
+
+  defp lengthCount([_ | tail], acc) do
+    acc = acc + 1
+
+    lengthCount(tail, acc)
   end
 end
